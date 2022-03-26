fbd3bfa968dad92cc63b68c327a8012cd90d7b36
ShyamaladeviG <sherlins2912@gmail.com>
2022-03-26 13:11:28 +0530

    Greenstech

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,36 @@
+package org.add;
+
+public class GreensTech {
+	
+private void greensOmr() {
+	System.out.println("omr");
+}
+private void greensAdyar() {
+	System.out.println("adyar");
+	}
+private void greensTambaram() {
+	System.out.println("tambaram");
+	}
+private void greensVelachery() {
+	System.out.println("velachery");
+	}
+private void greensAnnaNagar() {
+	System.out.println("anna nagar");
+}
+public static void main(String[] args) {
+
+	//ClassName objectName = new ClassName
+	GreensTech g = new GreensTech ();
+	
+	//method calling
+	
+	g.greensOmr();
+	g.greensAdyar();
+	g.greensTambaram();
+	g.greensVelachery();
+	g.greensAnnaNagar();
+}
+
+
+
+}
